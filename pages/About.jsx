36f21762b252f19b72de7a2f80fa34cf8ff6ff54
JoshHogan99 <52086873+JoshHogan99@users.noreply.github.com>
2024-03-02 20:44:26 +0000

    added more pages and information

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,12 +1,14 @@
 import React from "react"
+import { Link, NavLink } from "react-router-dom"
 
 import circle from "../assets/about/mobile/bg-pattern-hero-about-mobile.svg"
 import aboutHero from "../assets/about/mobile/image-about-hero.jpg"
 import worldClassTalent from "../assets/about/mobile/image-world-class-talent.jpg"
+import realDeal from "../assets/about/mobile/image-real-deal.jpg"
 import patternSmallCircle from "../assets/shared/desktop/bg-pattern-small-circle.svg"
-import illustrationPassionate from "../assets/shared/desktop/illustration-canada.svg"
-import illustrationResourceful from "../assets/shared/desktop/illustration-australia.svg"
-import illustrationFriendly from "../assets/shared/desktop/illustration-united-kingdom.svg"
+import illustrationCanada from "../assets/shared/desktop/illustration-canada.svg"
+import illustrationAustralia from "../assets/shared/desktop/illustration-australia.svg"
+import illustrationUnitedKingdom from "../assets/shared/desktop/illustration-united-kingdom.svg"
 
 export default function About(){
     return(
@@ -42,69 +44,121 @@ export default function About(){
                 className="about-img"
             />
 
+            <div className="about-us-info">
+                <p>World-class talent</p>
+
+                <p>
+                    We are a crew of strategists, problem-solvers, 
+                    and technologists. Every design is thoughtfully 
+                    crafted from concept to launch, ensuring success 
+                    in its given market. We are constantly updating 
+                    our skills in a myriad of platforms.
+                </p>
+
+                <p>
+                    Our team is multi-disciplinary and we are not 
+                    merely interested in form — content and meaning 
+                    are just as important. We give great importance 
+                    to craftsmanship, service, and prompt delivery. 
+                    Clients have always been impressed with our 
+                    high-quality outcomes that encapsulates their 
+                    brand's story and mission.
+                </p>
+            </div>
+
             <div className="illustrations">
+                <div className="illustration">
+                    <img 
+                        src={patternSmallCircle}
+                        className="illustrations-background canada-circle"
+                    />
+
+                    <img 
+                        src={illustrationCanada}
+                        className="canada-illustration"
+                    />
+
+                    <p>CANADA</p>
+
+                    <a 
+                        href="https://maps.app.goo.gl/NEHbJFaVUQRGCG4o6"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <button className="see-location-btn">SEE LOCATION</button>
+                    </a>
+                </div>
+
                 <div className="illustration">
                     <img 
                         src={patternSmallCircle}
                         className="illustrations-background"
                     />
-
-                    <img 
-                        src={illustrationPassionate}
-                        className="passionate-illustration"
-                    />
-
-                    <p>PASSIONATE</p>
-
-                    <p>
-                        Each project starts with an in-depth brand 
-                        research to ensure we only create products that 
-                        serve a purpose. We merge art, design, and 
-                        technology into exciting new solutions.
-                    </p>
-                </div>
-
-                <div className="illustration">
-                    <img 
-                        src={patternSmallCircle}
-                        className="illustrations-background resourceful-circle"
-                    />
                     
                     <img 
-                        src={illustrationResourceful}
-                        className="resourceful-illustration"
+                        src={illustrationAustralia}
+                        className="australia-illustration"
                     />
 
-                    <p>RESOURCEFUL</p>
+                    <p>AUSTRALIA</p>
 
-                    <p>
-                        Everything that we do has a strategic purpose.
-                        We use an agile approach in all of our projects 
-                        and value customer collaboration. It guarantees 
-                        superior results that fulfill our clients' needs.
-                    </p>
+                    <a 
+                        href="https://maps.app.goo.gl/M8mBAgjW9My3nhYC6"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <button className="see-location-btn">SEE LOCATION</button>
+                    </a>
                 </div>
 
                 <div className="illustration">
                     <img 
                         src={patternSmallCircle}
-                        className="illustrations-background friendly-circle"
+                        className="illustrations-background united-kingdom-circle"
                     />
 
                     <img 
-                        src={illustrationFriendly}
-                        className="friendly-illustration"
+                        src={illustrationUnitedKingdom}
+                        className="united-kingdom-illustration"
                     />
 
-                    <p>FRIENDLY</p>
+                    <p>UNITED KINGDOM</p>
 
-                    <p>
-                        We are a group of enthusiastic folks who know 
-                        how to put people first. Our success depends on 
-                        our customers, and we strive to give them the 
-                        best experience a company can provide.
-                    </p>
+                    <a 
+                        href="https://maps.app.goo.gl/qERBr67VUcdF4GdZ6"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <button className="see-location-btn">SEE LOCATION</button>
+                    </a>
                 </div>
+            </div>
+
+            <img 
+                src={realDeal}
+                alt="woman staring at images on wall"
+                className="about-img"
+            />
+
+            <div className="about-us-info">
+                <p>The real deal</p>
+
+                <p>
+                    As strategic partners in our clients' business, 
+                    we are ready to take on any challenge as our own. 
+                    Solving real problems require empathy and 
+                    collaboration, and we strive to bring a fresh 
+                    perspective to every opportunity. We make design 
+                    and technology more accessible and give you tools 
+                    to measure success.
+                </p>
+
+                <p>
+                    We are visual storytellers in appealing and 
+                    captivating ways. By combinining business and 
+                    marketing strategies, we inspire audiences to 
+                    take action and drive real results.
+                </p>
             </div>
         </div>
     )
--- pages/Locations.jsx
+++ pages/Locations.jsx
@@ -1,9 +1,89 @@
 import React from "react"
 
+import canadaMapImg from "../assets/locations/desktop/image-map-canada.png"
+import australiaMapImg from "../assets/locations/desktop/image-map-australia.png"
+import unitedKingdomMapImg from "../assets/locations/desktop/image-map-united-kingdom.png"
+
 export default function Locations(){
     return(
         <div className="locations">
-            locations here
+            <img 
+                src={canadaMapImg}
+                alt="woman staring at images on wall"
+                className="about-img"
+            />
+
+            <div className="locations-info">
+                <p className="locations-heading">Canada</p>
+
+                <div className="location-info">
+                    <span>Designo Central Office</span>
+
+                    <p>3886 Wellington Street</p>
+
+                    <p>Toronto, Ontario M9C 3J5</p>
+                </div>
+
+                <div className="location-info">
+                    <span>Contact</span>
+
+                    <p>P : +1 253-863-8967</p>
+
+                    <p>M : contact@designo.co</p>
+                </div>
+            </div>
+
+            <img 
+                src={australiaMapImg}
+                alt="woman staring at images on wall"
+                className="about-img"
+            />
+
+            <div className="locations-info">
+                <p className="locations-heading">Australia</p>
+
+                <div className="location-info">
+                    <span>Designo AU Office</span>
+
+                    <p>19 Balonne Street</p>
+
+                    <p>New South Wales 2443</p>
+                </div>
+
+                <div className="location-info">
+                    <span>Contact</span>
+
+                    <p>P : (02) 6720 9092</p>
+
+                    <p>M : contact@designo.au</p>
+                </div>
+            </div>
+
+            <img 
+                src={unitedKingdomMapImg}
+                alt="woman staring at images on wall"
+                className="about-img"
+            />
+
+            <div className="locations-info">
+                <p className="locations-heading">United Kingdom</p>
+
+                <div className="location-info">
+                    <span>Designo UK Office</span>
+
+                    <p>13 Colorado Way</p>
+
+                    <p>Rhyd-y-fro SA8 9GA</p>
+                </div>
+
+                <div className="location-info">
+                    <span>Contact</span>
+
+                    <p>P : 078 3115 1400</p>
+
+                    <p>M : contact@designo.uk</p>
+                </div>
+            </div>
         </div> 
     )
 }
