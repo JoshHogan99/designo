15e7d6b5c6934fa762262608127986ca19c69965
JoshHogan99 <52086873+JoshHogan99@users.noreply.github.com>
2024-03-08 20:53:52 +0000

    Added more general CSS and media queries
    Finished CSS media queries for tablet design and added a missing background circle location page

--- pages/Locations.jsx
+++ pages/Locations.jsx
@@ -4,84 +4,151 @@ import canadaMapImg from "../assets/locations/desktop/image-map-canada.png"
 import australiaMapImg from "../assets/locations/desktop/image-map-australia.png"
 import unitedKingdomMapImg from "../assets/locations/desktop/image-map-united-kingdom.png"
 
+import canadaMapImgTablet from "../assets/locations/tablet/image-map-canada.png"
+import australiaMapImgTablet from "../assets/locations/tablet/image-map-australia.png"
+import unitedKingdomMapImgTablet from "../assets/locations/tablet/image-map-uk.png"
+
+import circle from "../assets/shared/desktop/bg-pattern-small-circle.svg"
+import twoCircles from "../assets/shared/desktop/bg-pattern-two-circles.svg"
+
 export default function Locations(){
     return(
         <div className="locations">
             <img 
                 src={canadaMapImg}
-                alt="woman staring at images on wall"
-                className="about-img"
+                alt="canada google maps"
+                className="location-img"
+            />
+
+            <img 
+                src={canadaMapImgTablet}
+                alt="canada google maps"
+                className="location-img-tablet"
             />
 
             <div className="locations-info">
+                <img 
+                    src={circle}
+                    alt="background oval" 
+                    className="locations-bg-img"
+                />
+
+                <img 
+                    src={twoCircles}
+                    alt="two circles background"
+                    className="locations-bg-img-tablet"
+                />
+
                 <p className="locations-heading">Canada</p>
 
-                <div className="location-info">
-                    <span>Designo Central Office</span>
+                <div className="location-info-container">
+                    <div className="location-info">
+                        <span>Designo Central Office</span>
 
-                    <p>3886 Wellington Street</p>
+                        <p>3886 Wellington Street</p>
 
-                    <p>Toronto, Ontario M9C 3J5</p>
-                </div>
+                        <p>Toronto, Ontario M9C 3J5</p>
+                    </div>
 
-                <div className="location-info">
-                    <span>Contact</span>
+                    <div className="location-info">
+                        <span>Contact</span>
 
-                    <p>P : +1 253-863-8967</p>
+                        <p>P : +1 253-863-8967</p>
 
-                    <p>M : contact@designo.co</p>
+                        <p>M : contact@designo.co</p>
+                    </div>
                 </div>
             </div>
 
             <img 
                 src={australiaMapImg}
                 alt="woman staring at images on wall"
-                className="about-img"
+                className="location-img"
+            />
+
+            <img 
+                src={australiaMapImgTablet}
+                alt="australia google maps"
+                className="location-img-tablet"
             />
 
             <div className="locations-info">
+                <img 
+                    src={circle}
+                    alt="background oval" 
+                    className="locations-bg-img"
+                />
+
+                <img 
+                    src={twoCircles}
+                    alt="two circles background"
+                    className="locations-bg-img-tablet"
+                />
+
                 <p className="locations-heading">Australia</p>
 
-                <div className="location-info">
-                    <span>Designo AU Office</span>
+                <div className="location-info-container">
+                    <div className="location-info">
+                        <span>Designo AU Office</span>
 
-                    <p>19 Balonne Street</p>
+                        <p>19 Balonne Street</p>
 
-                    <p>New South Wales 2443</p>
-                </div>
+                        <p>New South Wales 2443</p>
+                    </div>
 
-                <div className="location-info">
-                    <span>Contact</span>
+                    <div className="location-info">
+                        <span>Contact</span>
 
-                    <p>P : (02) 6720 9092</p>
+                        <p>P : (02) 6720 9092</p>
 
-                    <p>M : contact@designo.au</p>
+                        <p>M : contact@designo.au</p>
+                    </div>
                 </div>
             </div>
 
             <img 
                 src={unitedKingdomMapImg}
                 alt="woman staring at images on wall"
-                className="about-img"
+                className="location-img"
+            />
+
+            <img 
+                src={unitedKingdomMapImgTablet}
+                alt="united kingdom google maps"
+                className="location-img-tablet"
             />
 
             <div className="locations-info">
+                <img 
+                    src={circle}
+                    alt="background oval" 
+                    className="locations-bg-img"
+                />
+
+                <img 
+                    src={twoCircles}
+                    alt="two circles background"
+                    className="locations-bg-img-tablet"
+                />
+
                 <p className="locations-heading">United Kingdom</p>
 
-                <div className="location-info">
-                    <span>Designo UK Office</span>
+                <div className="location-info-container">
+                    <div className="location-info">
+                        <span>Designo UK Office</span>
 
-                    <p>13 Colorado Way</p>
+                        <p>13 Colorado Way</p>
 
-                    <p>Rhyd-y-fro SA8 9GA</p>
-                </div>
+                        <p>Rhyd-y-fro SA8 9GA</p>
+                    </div>
 
-                <div className="location-info">
-                    <span>Contact</span>
+                    <div className="location-info">
+                        <span>Contact</span>
 
-                    <p>P : 078 3115 1400</p>
+                        <p>P : 078 3115 1400</p>
 
-                    <p>M : contact@designo.uk</p>
+                        <p>M : contact@designo.uk</p>
+                    </div>
                 </div>
             </div>
         </div> 
